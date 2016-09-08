9411fce689ed561fff658ad00bf22959cabefaca
Matt Long <rolovis@gmail.com>
2016-09-08 16:54:11 -0700

    Moved some stuff around

--- abilities.py
+++
@@ -1,9 +0,0 @@
-class Ability(object):
-
-    def __init__(self, name):
-        self.name = name
-
-    def use(self, player):
-        pass
-
-# test
--- game.py
+++ game.py
@@ -4,21 +4,22 @@ import items
 
 george = character.Character('George')
 print(george.get_stats())
-george.add_stats({'strength': 10})
 george = character.gen(george)
+george.add_stats({'strength': 10})
 george.add_level()
 george.add_level()
 print(george)
 
 george.equip(items.sword_of_crushing)
 george.add_skill('acrobatics')
+print(george.class_skills)
 print(george.equipment['weapon'])
 
 print('Break down the door!')
 input('ready?')
 
 # if mechanics.attack(george, george, False) > 3:
-if mechanics.ability_check(15, 'strength', george):
+if mechanics.ability_check(15, 'strength', george, 'acrobatics'):
     print('The door explodes.')
 else:
     print('You broke your foot. Good job asshole.')

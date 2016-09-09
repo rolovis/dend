3b20c5e00f1803e16261696121687023f3b43b1e
Rolovis <rolovis@gmail.com>
2016-09-08 19:04:27 -0700

    Moved name entry from gen()

--- game.py
+++ game.py
@@ -1,25 +1,26 @@
+#!python3
+
 import character
 import mechanics
 import items
 
-george = character.Character('George')
-print(george.get_stats())
-george = character.gen(george)
-george.add_stats({'strength': 10})
-george.add_level()
-george.add_level()
-print(george)
+player_name = input('Enter your name:')
+player = character.Character(player_name)
+player = character.gen(player)
+player.add_stats({'strength': 10})
+player.add_level()
+player.add_level()
+print(player)
 
-george.equip(items.sword_of_crushing)
-george.add_skill('acrobatics')
-print(george.class_skills)
-print(george.equipment['weapon'])
+player.equip(items.sword_of_crushing)
+player.add_skill('acrobatics')
+print(player.class_skills)
+print(player.equipment['weapon'])
 
 print('Break down the door!')
 input('ready?')
 
-# if mechanics.attack(george, george, False) > 3:
-if mechanics.ability_check(15, 'strength', george, 'acrobatics'):
+if mechanics.ability_check(15, 'strength', player, 'acrobatics'):
     print('The door explodes.')
 else:
     print('You broke your foot. Good job asshole.')

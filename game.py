70c640cb6ab079eca7a48a3c4854382890831cc9
Rolovis <rolovis@gmail.com>
2016-09-09 19:25:39 -0700

    Cleaned some stuff up

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ import character
 import mechanics
 import items
 
-player_name = input('Enter your name:')
+player_name = input('Enter your name:\n')
 player = character.Character(player_name)
 player = character.gen(player)
 player.add_stats({'strength': 10})
@@ -13,14 +13,12 @@ player.add_level()
 print(player)
 
 player.equip(items.sword_of_crushing)
-player.add_skill('acrobatics')
-print(player.class_skills)
 print(player.equipment['weapon'])
 
 print('Break down the door!')
 input('ready?')
 
-if mechanics.ability_check(15, 'strength', player, 'acrobatics'):
+if mechanics.ability_check(15, 'strength', player, 'history'):
     print('The door explodes.')
 else:
     print('You broke your foot. Good job asshole.')
